f435f321c02f73ba66af3d087616ab1839e450ce
Manoel317 <147872069+Manoel317@users.noreply.github.com>
2026-04-10 03:10:05 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "medidor-site-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+
+  "/arthur.gif",
+  "/joaolucas.png",
+  "/sixseven.jpg",
+  "/Matheus .mov",
+
+  "/67.mp3",
+  "/Beat.mp3",
+  "/Inst.mp3",
+  "/PH.mp3",
+  "/Queima.mp3",
+  "/ai-meu-cxzinhx.mp3",
+  "/giga-chad-theme.mp3",
+  "/redline.mp3",
+  "/ui.mp3",
+  "/ze da manga.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
